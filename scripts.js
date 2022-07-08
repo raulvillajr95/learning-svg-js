880cda2cac7486c55322a888627de36744483556
Raul <raulvillajr95@gmail.com>
2022-07-07 21:36:31 -0700

    Rearrange svg rectangles

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
-let draw = SVG("lines").size(500,500);
+let draw = SVG('placement').size(500, 500)
 
-let line = draw.line([[40,40],[400,400]]).stroke({width: 2})
+let rect1 = draw.rect(200,100).attr({fill: "#f03"})
+let rect2 = draw.rect(100,200).attr({fill: "#0f5"})
+let rect3 = draw.rect(200,100).attr({fill: "#4af"}).y(100)
+
+rect1.before(rect3)
